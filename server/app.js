9768f7c2ca95edd55bbed2c911181df3b8c7abd0
Akib <akib7599@gmail.com>
2023-03-20 11:41:36 +0530

    done backend structer

---
+++ server/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express()
+const port = 8005
+//require("./db/conn")
+const cors = require('cors')
+//const router = require('./routes/router')
+
+
+app.use(express.json())
+app.use(cors())
+//app.use(router)
+
+
+
+
+app.listen(port,()=>{
+    console.log(`your server start on port number ${port}`)
+})
